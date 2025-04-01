9b403f83b2238aa609824c7e9ae8fcce651413e1
Qantals_book <aheng_ah@126.com>
2025-04-01 17:20:41 +0800

    first commit

---
+++ crc5.v
@@ -0,0 +1,22 @@
+module crc5(c, d, c_out);
+input	[4:0]	c;
+input	[10:0]	d;
+output	[4:0]	c_out;
+
+assign c_out[0] =	d[10] ^ d[9] ^ d[6] ^ d[5] ^ d[3] ^
+			d[0] ^ c[0] ^ c[3] ^ c[4];
+
+assign c_out[1] =	d[10] ^ d[7] ^ d[6] ^ d[4] ^ d[1] ^
+			c[0] ^ c[1] ^ c[4];
+
+assign c_out[2] =	d[10] ^ d[9] ^ d[8] ^ d[7] ^ d[6] ^
+			d[3] ^ d[2] ^ d[0] ^ c[0] ^ c[1] ^
+			c[2] ^ c[3] ^ c[4];
+
+assign c_out[3] =	d[10] ^ d[9] ^ d[8] ^ d[7] ^ d[4] ^ d[3] ^
+			d[1] ^ c[1] ^ c[2] ^ c[3] ^ c[4];
+
+assign c_out[4] =	d[10] ^ d[9] ^ d[8] ^ d[5] ^ d[4] ^ d[2] ^
+			c[2] ^ c[3] ^ c[4];
+
+endmodule
